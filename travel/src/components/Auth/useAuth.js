3df7391cbe374f13b2e9b3a253a2b549b376c828
El Amraoui Khalil <khalilelam16@gmail.com>
2024-08-29 21:19:37 +0100

    cleanup

--- travel/src/components/Auth/useAuth.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { AuthContext } from '../../hooks/AuthProvider'; // Adjust path if needed
-
-export const useAuth = () => {
-    return useContext(AuthContext);
-};
