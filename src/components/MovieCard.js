e15a61555d4557c5509869148c3f9ce6985443fc
Clear008 <wissal-exo@hotmail.com>
2024-09-06 17:51:17 +0100

    Edit the movie card botton

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -16,7 +16,7 @@ function MovieCard({ movie }) {
   };
 
   return (
-    <Link to={`/movie/${movie.id}`} className="card bg-red-600">
+    <Link to={`/movie/${movie.id}`} className="card">
       <img 
         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
         alt={movie.title}
